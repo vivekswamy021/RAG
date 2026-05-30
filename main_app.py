1e31e10379d70451e42f74c20188093790a442db
VIVEK <vivekswamy021@gmail.com>
2026-05-30 14:19:54 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -131,7 +131,7 @@ if user_query:
         # Call the Supabase SQL function directly
         response = supabase.rpc(
             "match_documents", 
-            {"query_embedding": query_vector, "match_count": 3}
+            {"query_embedding": query_vector, "match_count": 5}
         ).execute()
         
         st.info(f"Database found {len(response.data)} matching paragraphs.")
